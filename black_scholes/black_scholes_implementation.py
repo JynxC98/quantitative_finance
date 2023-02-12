4a4cfecdf4d2e87190fa04cf68f0cddf45ced36d
JynxC98 <parikh.harsh.hp@gmail.com>
2023-02-12 23:24:48 +0000

    added black scholes implementation file

---
+++ black_scholes/black_scholes_implementation.py
@@ -0,0 +1,13 @@
+"""
+Implementation of Black-Scholes model
+"""
+import warnings
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+warnings.filterwarnings("ignore")
+
+if __name__ == "__main__":
+    pass
