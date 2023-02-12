d6c1e45ac182b641968277dbaa81e47f882b4034
JynxC98 <parikh.harsh.hp@gmail.com>
2023-02-12 23:51:31 +0000

    added implementation of call and put option prices

--- black_scholes/black_scholes_implementation.py
+++ black_scholes/black_scholes_implementation.py
@@ -3,11 +3,56 @@ Implementation of Black-Scholes model
 """
 import warnings
 import numpy as np
+from scipy import stats
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
 warnings.filterwarnings("ignore")
 
+
+def call_option_price(initial_price, strike_price, T, rf, sigma):
+    """
+    Parameters:
+    -----------
+    S: Initial stock price
+    E: Strike Price
+    T: Time to maturity
+    rf: Risk Free interest rate
+    sigma: Volatility
+    """
+    d1 = (np.log(initial_price / strike_price) + (rf + pow(sigma, 2) / 2) * T) / (
+        sigma * np.sqrt(T)
+    )
+
+    d2 = d1 - sigma * np.sqrt((T))
+
+    return initial_price * stats.norm.cdf(d1) - strike_price * np.exp(
+        -rf * T
+    ) * stats.norm.cdf(d2)
+
+
+def put_option_price(initial_price, strike_price, T, rf, sigma):
+    """
+    Parameters:
+    -----------
+    initial_price: Initial stock price
+    strike_price: Strike Price
+    T: Time to maturity
+    rf: Risk Free interest rate
+    sigma: Volatility
+    """
+    d1 = (np.log(initial_price / strike_price) + (rf + pow(sigma, 2) / 2) * T) / (
+        sigma * np.sqrt(T)
+    )
+
+    d2 = d1 - sigma * np.sqrt((T))
+
+    return -initial_price * stats.norm.cdf(-d1) + strike_price * np.exp(
+        -rf * T
+    ) * stats.norm.cdf(-d2)
+
+
 if __name__ == "__main__":
-    pass
+    print(call_option_price(100, 100, 1, 0.05, 0.2))
+    print(put_option_price(100, 100, 1, 0.05, 0.2))
