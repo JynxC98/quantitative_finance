251a703bba4450fb25318b92fdb6ac0df98f27d1
JynxC98 <parikh.harsh.hp@gmail.com>
2023-03-07 00:31:49 +0000

    added incomplete implementation of Monte-Carlo simulation

---
+++ black_scholes/monte-carlo-simulation.py
@@ -0,0 +1,53 @@
+"""Simulation of monte carlo simulation
+"""
+
+import collections
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# External Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+NUM_SIMULATIONS = 100
+
+
+def stock_monete_carlo(
+    S_0: float, mu: float, sigma: float, n_iter: int = 1000
+) -> pd.DataFrame:
+    """
+    Implementation of monte-carlo simulation
+    Parameters
+    ----------
+    mu: Mean of the stock
+    sigma: Standard deviation of the stock
+    n_iter: number of iterations
+    S_0: Initial price of the stock.
+
+    Output
+    ------
+    Graph of several simulations and the mean of the simulations
+    """
+
+    result = []
+
+    for _ in range(NUM_SIMULATIONS):
+        prices = [S_0]
+
+        for _ in range(n_iter):
+            stock_price = prices[-1] * np.exp(
+                (mu - 0.5 * pow(sigma, 2)) + sigma * np.random.normal()
+            )
+
+            prices.append(stock_price)
+
+        result.append(prices)
+
+    simulation_data = pd.DataFrame(result).T
+
+    return simulation_data
+
+    #
