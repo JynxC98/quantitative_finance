42e2a9e9c05ea1b53cf1df8ce172e3350b4c7c6e
JynxC98 <parikh.harsh.hp@gmail.com>
2023-04-13 21:42:30 +0100

    converted the simulation function into **kwargs format

--- monte-carlo-sim/models.py
+++ monte-carlo-sim/models.py
@@ -5,19 +5,7 @@ Uses Euler scheme
 import numpy as np
 
 
-def simulate_heston_model_euler(
-    S_0,
-    v_0,
-    theta,
-    sigma,
-    kappa,
-    rho,
-    risk_free_rate,
-    time_to_maturity,
-    strike_price,
-    num_paths,
-    step_size,
-) -> dict:
+def simulate_heston_model_euler(**kwargs) -> dict:
     """
     Simulation of Heston Model under euler scheme.
 
@@ -40,6 +28,20 @@ def simulate_heston_model_euler(
     Euler call option price
     Euler confidence interval
     """
+    S_0 = kwargs.get("S_0", 100)
+    v_0 = kwargs.get("v_0", 0.09)
+    theta = kwargs.get("theta", 0.348)
+    sigma = kwargs.get("sigma", 0.39)
+    kappa = kwargs.get("kappa", 1.15)
+    rho = kwargs.get("rho", -0.64)
+    risk_free_rate = kwargs.get("risk_free_rate", 0.05)
+    time_to_maturity = kwargs.get("time_to_maturity", 1)
+    strike_price = kwargs.get("strike_price", 90)
+    num_paths = kwargs.get("num_paths", 500000)
+    step_size = kwargs.get("step_size", 10e-3)
+
+    if len(kwargs) < 11:
+        raise ValueError("Have you entered all the arguments? \n")
 
     num_iterations = int(time_to_maturity / step_size)
     total_stock_price = S_0 * np.ones([num_paths, 1])
@@ -76,9 +78,9 @@ def simulate_heston_model_euler(
 
     std_payoff = np.std(payoff)
     call_euler = np.mean(payoff)
-    V1left = call_euler - 1.96 * std_payoff / np.sqrt(num_paths)
-    V1right = call_euler + 1.96 * std_payoff / np.sqrt(num_paths)
-    confidence_interval = tuple([V1left, V1right])
+    V1_left = call_euler - 1.96 * std_payoff / np.sqrt(num_paths)
+    V1_right = call_euler + 1.96 * std_payoff / np.sqrt(num_paths)
+    confidence_interval = tuple([V1_left, V1_right])
 
     return {
         "Mean Call Option Price": call_euler,
