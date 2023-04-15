8490105a10b3412284bda9263a421b5f26b3eaed
JynxC98 <parikh.harsh.hp@gmail.com>
2023-04-15 18:07:03 +0100

    2nd draft of implied volatility

--- implied-volatility/implied_volatility_implementation.py
+++ implied-volatility/implied_volatility_implementation.py
@@ -7,8 +7,19 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 from scipy.stats import norm
+from scipy.optimize import brentq
 
 warnings.filterwarnings("ignore")
+
+
+def get_data_from_yahoo(start_date, end_date, ticker) -> np.array:
+    """
+    start_date: Historical start date of the data
+    end_date: Historical end date of the data
+    ticker: Ticker of the company needed to calculate the option data
+    """
+    data = yf.download(ticker, start_date, end_date)
+    return np.log(1 + data["Adj Close"].pct_change())
 
 
 class ImpliedVolatility:
@@ -23,7 +34,11 @@ class ImpliedVolatility:
     time: Time to maturity
     sigma: Volatility at time t
     option_type: Call (1) or put(0) option.
+    start: starting date
+    end: ending date
     """
+
+    OPTION_PRICE = 0
 
     def __init__(self, **kwargs) -> None:
         """
@@ -32,6 +47,8 @@ class ImpliedVolatility:
         if len(kwargs) != 6:
             raise ValueError("Incorrect number of arguments")
 
+        self.start = kwargs.get("start")
+        self.end = kwargs.get("end")
         self.stock_price = kwargs.get("stock_price")
         self.risk_free_rate = kwargs.get("risk_free_rate")
         self.strike_price = kwargs.get("strike_price")
@@ -62,9 +79,10 @@ class ImpliedVolatility:
                 -self.risk_free_rate * self.time
             ) * norm.cdf(-d_2) - self.stock_price * norm.cdf(-d_1)
 
-    def get_data_from_yahoo(self, start_date, end_date, ticker) -> None:
+    def calculate_implied_volatility(self):
         """
-        start_date: Historical start date of the data
-        end_date: Historical end date of the data
-        ticker: Ticker of the company needed to calculate the option data
+        Uses Newton's iterative method to calculate implied volatility.
         """
+        return brentq(
+            lambda x: self.black_scholes_merton_price() - self.OPTION_PRICE, 0.00001, 2
+        )
