4d9ae9cbef0bbec6ddb0a249af4ce96a64c3e7bd
JynxC98 <parikh.harsh.hp@gmail.com>
2023-04-23 14:18:50 +0100

    added draft of milstein model

--- monte-carlo-sim/models.py
+++ monte-carlo-sim/models.py
@@ -89,6 +89,54 @@ def simulate_heston_model_euler(**kwargs) -> dict:
     }
 
 
+def simulate_heston_model_milstein(**kwargs) -> dict:
+    """
+    Simulation of Heston Model under Milstein scheme.
+
+    Input Parameters
+    ----------------
+    S_0: Stock price at t = 0
+    v_0: Volatility at t = 0
+    theta: long run average of the volatility
+    sigma: volatility of volatility
+    kappa: rate of mean reversion
+    rho: correlation between two brownian motions
+    risk_free_rate: Interest rate of riskless assets
+    time_to_maturity: Option's contract period. Measured in years.
+    strike_price: Option's strike price
+    num_paths: Number of paths in Brownian motion
+    step_size: Time Increments
+
+    Returns
+    -------
+    Milstein call option price
+    Milstein confidence interval
+    """
+    S_0 = kwargs.get("S_0", 100)
+    v_0 = kwargs.get("v_0", 0.09)
+    theta = kwargs.get("theta", 0.348)
+    sigma = kwargs.get("sigma", 0.39)
+    kappa = kwargs.get("kappa", 1.15)
+    rho = kwargs.get("rho", -0.64)
+    risk_free_rate = kwargs.get("risk_free_rate", 0.05)
+    time_to_maturity = kwargs.get("time_to_maturity", 1)
+    strike_price = kwargs.get("strike_price", 90)
+    num_paths = kwargs.get("num_paths", 500000)
+    step_size = kwargs.get("step_size", 10e-3)
+
+    if len(kwargs) != 11:
+        raise ValueError("Have you entered correct number of arguments? \n")
+
+    num_iterations = int(time_to_maturity / step_size)
+    total_stock_price = S_0 * np.ones([num_paths, 1])
+
+    stock_price = S_0 * np.ones([num_paths, 1])
+    volatility = v_0 * np.ones([num_paths, 1])
+
+    # Seed generates the same brownian motion
+    np.random.seed(1)
+
+
 if __name__ == "__main__":
     start_time = time.time()
     print(
