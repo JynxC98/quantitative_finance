4a340baa75b026b3360f137d9197e0099a01da71
JynxC98 <parikh.harsh.hp@gmail.com>
2023-11-10 14:43:52 +0530

    updated the code of implied volatility

--- implied-volatility/implied_volatility_implementation.py
+++ implied-volatility/implied_volatility_implementation.py
@@ -1,88 +1,61 @@
 """
-Script to simulate implied volatility
+A script to calculate implied volatility.
 """
-import warnings
-import plotly.express as px
 import numpy as np
-import pandas as pd
-import yfinance as yf
 from scipy.stats import norm
-from scipy.optimize import brentq
-
-warnings.filterwarnings("ignore")
 
 
-def get_data_from_yahoo(start_date, end_date, ticker) -> np.array:
+def implied_volatility(S, K, r, T, actual_price, callput):
     """
-    start_date: Historical start date of the data
-    end_date: Historical end date of the data
-    ticker: Ticker of the company needed to calculate the option data
-    """
-    data = yf.download(ticker, start_date, end_date)
-    return np.log(1 + data["Adj Close"].pct_change())
-
-
-class ImpliedVolatility:
-    """
-    Implied volatility calculation using BSM model
+    Calculate implied volatility using Newton's iterative formula.
 
     Parameters
     ----------
-    stock_price: Stock Price at time t
-    risk_free_rate: Risk free interest rate
-    strike_price: Strike price at time T
-    time: Time to maturity
-    sigma: Volatility at time t
-    option_type: Call (1) or put(0) option.
-    start: starting date
-    end: ending date
+    S : float
+        Stock price.
+    K : float
+        Strike price.
+    r : float
+        Risk-free rate.
+    T : float
+        Time to maturity.
+    actual_price : float
+        Option price.
+    callput : int
+        Type of option. 1 for Call, 0 for Put.
+
+    Returns
+    -------
+    float
+        Implied volatility.
     """
+    imp_vol = 1  # Initial guess
+    max_iterations = 1000
+    tolerance = 1e-5
+    option_price = 0
 
-    OPTION_PRICE = 0
+    for _ in range(max_iterations):
+        sigma = imp_vol
+        d1 = (np.log(S / K) + (r + (sigma**2) / 2) * T) / (sigma * np.sqrt(T))
+        d2 = d1 - sigma * np.sqrt(T)
+        if callput == 1:  # Call option
+            option_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+        elif callput == 0:  # Put option
+            option_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
 
-    def __init__(self, **kwargs) -> None:
-        """
-        Initialisation function of the class `Implied Volatility`.
-        """
-        if len(kwargs) != 6:
-            raise ValueError("Incorrect number of arguments")
+        f_sigma = option_price - actual_price  # To calculate implied volatility
+        vega = S * np.sqrt(T) * norm.pdf(d1)
+        imp_vol = sigma - f_sigma / vega
+        if abs(sigma - imp_vol) <= tolerance:
+            break
 
-        self.start = kwargs.get("start")
-        self.end = kwargs.get("end")
-        self.stock_price = kwargs.get("stock_price")
-        self.risk_free_rate = kwargs.get("risk_free_rate")
-        self.strike_price = kwargs.get("strike_price")
-        self.time = kwargs.get("time")
-        self.sigma = kwargs.get("sigma")
-        self.option_type = kwargs.get("option_type")
+    return imp_vol
 
-        if self.option_type not in (0, 1):
-            raise ValueError("Incorrect input for option_type")
 
-    def black_scholes_merton_price(self) -> float:
-        """
-        Calculates the value of option using BSM pricing formula.
-        """
-        d_1 = (
-            np.log(self.stock_price / self.strike_price)
-            + (self.risk_free_rate + 0.5 * pow(self.sigma, 2))
-        ) / (self.sigma * np.sqrt(self.time))
-
-        d_2 = d_1 - self.sigma * np.sqrt(self.time)
-
-        if self.option_type == 1:
-            return self.stock_price * norm.cdf(d_1) - self.strike_price * np.exp(
-                -self.risk_free_rate * self.time
-            ) * norm(d_2)
-        else:
-            return self.strike_price * np.exp(
-                -self.risk_free_rate * self.time
-            ) * norm.cdf(-d_2) - self.stock_price * norm.cdf(-d_1)
-
-    def calculate_implied_volatility(self):
-        """
-        Uses Newton's iterative method to calculate implied volatility.
-        """
-        return brentq(
-            lambda x: self.black_scholes_merton_price() - self.OPTION_PRICE, 0.00001, 2
-        )
+if __name__ == "__main__":
+    S = 100
+    RISK_FREE = 0.05
+    K = np.linspace(80, 120, 20)
+    # implied_volatility_values = np.zeros([len(K)])
+    # for itr, strike_price in enumerate(K):
+    #     implied_volatility_values[itr] = implied_volatility(S, strike_price, RISK_FREE, )
