f593637f0e72957ec5ee2ac316079467179dc27d
Harsh Parikh <parikh.harsh.hp@gmail.com>
2025-02-18 20:04:26 +0530

    added the first draft of the Hull-White model and included the SOFR data

---
+++ hull-white-model/models.py
@@ -0,0 +1,27 @@
+"""
+A script to calculate the Hull White model using the affine term structure
+formula.
+
+Author: Harsh Parikh 
+Date: 18-02-2025
+"""
+
+import numpy as np
+from numba import jit
+
+
+class HullWhite:
+    """ """
+
+    def __init__():
+        """ """
+
+    def _calculate_theta():
+        """ """
+
+    def fit():
+        """ """
+
+
+if __name__ == "__main__":
+    pass
