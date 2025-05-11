edeb100f04b15d248303492ea5010c0cd6009c80
Harsh Parikh <parikh.harsh.hp@gmail.com>
2025-05-11 17:55:21 +0530

    added a folder for k-means clustering and added the base files for the implementations

---
+++ machine-learning-implementations/k-means/models.py
@@ -0,0 +1,15 @@
+"""
+Implementation of k-means clustering from scratch using numpy extensively.
+
+Author: Harsh Parikh
+Date: 11th May 2025
+"""
+
+import numpy as np
+
+
+class KMEans:
+    """ """
+
+    def __init__(self, num_clusters_):
+        """ """
